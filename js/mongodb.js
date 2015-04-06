437c2c17d742e4e04a426a62f7bec49d17ed5cf5
Marcel Maatkamp <m.maatkamp@gmail.com>
2015-04-06 07:35:18 +0200

    new

--- js/mongodb.js
+++ js/mongodb.js
@@ -2,24 +2,24 @@ var amqp = require('amqp')
 
 var MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
-var mongodb_url = process.env.MONGODB_URL || 'mongodb://localhost:27017/myproject';
+var mongodb_url = process.env.MONGODB_URL || 'mongodb://mongodb:27017/gsm';
 
 var mongodb = MongoClient.connect(mongodb_url, function(err, db) {
-  var collection = db.collection('dns');
+  var collection = db.collection('gsm');
   assert.equal(null, err);
   console.log("Connected successfully on " + mongodb_url);
-  var rabbitmq_url = process.env.AMQP_URL || "amqp://localhost"
+  var rabbitmq_url = process.env.AMQP_URL || "amqp://rabbitmq"
   var conn = amqp.createConnection({ url: rabbitmq_url }, {
     reconnect: true,
     reconnectBackoffStrategy: 'linear',
     reconnectBackoffTime: 500,
-    defaultExchangeName: "dns"
+    defaultExchangeName: "gsm"
   })
   conn.on('ready', function() { 
     console.log("connected on " + rabbitmq_url)
     var exchange = conn.exchange('gsm');
     conn.queue('gsm_mongodb', { durable: true }, function(queue) {
-      queue.bind(exchange, 'dns')
+      queue.bind(exchange, 'gsm')
       queue.subscribe(function(msg) {
         collection.insert(msg, function(err, result) {
           assert.equal(null, err);
